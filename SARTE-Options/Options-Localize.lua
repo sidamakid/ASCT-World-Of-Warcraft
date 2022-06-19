07894bca2686552422aadb70eb2615af1e69fe9a
sidamakid <38231721+sidamakid@users.noreply.github.com>
2022-06-19 11:26:40 -0700

    Add files via upload

---
+++ SARTE-Options/Options-Localize.lua
@@ -0,0 +1,6 @@
+local function defaultFunc(L, key)
+ -- If this function was called, we have no localization for this key.
+ -- We could complain loudly to allow localizers to see the error of their ways, but, for now, just return the key as its own localization. This allows you to avoid writing the default localization out explicitly.
+ return key;
+end
+SARTE_Localization_My_Localization_Table = setmetatable({}, {__index=defaultFunc});
