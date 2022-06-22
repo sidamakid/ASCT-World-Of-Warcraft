18e88179380bd27b01bf3db2adc6cf78368349cf
sidamakid <38231721+sidamakid@users.noreply.github.com>
2022-06-21 17:41:46 -0700

    Add files via upload

---
+++ SARTE-Options/SARTE-Options/Localization/Localize-ptBR.lua
@@ -0,0 +1,51 @@
+local L = SARTE_Localization_My_Localization_Table;
+if GetLocale() == "ptBR	" then
+ --Rogue
+ L["Rogue"] = "Velhaco";
+ L["Assassination"] = "Assassinato";
+ L["Combat"] = "Combate";
+ L["Outlaw"] = "Bandido";
+ L["Subtlety"] = "Sutileza";
+ --Priest
+ L["Priest"] = "Sacerdote";
+ L["Shadow"] = "Sombra";
+ L["Discipline"] = "Disciplina";
+ L["Racials_Priest"] = "Padre Racial";
+ --Warrior
+ L["Warrior"] = "Guerreiro";
+ L["Arms"] = "Armas";
+ L["Fury"] = "Fúria";
+ --Druid
+ L["Druid"] = "Druida";
+ L["Balance"] = "Equilíbrio";
+ L["Feral_Combat"] = "Combate_Feroz";
+ --Shaman
+ L["Shaman"] = "Xamã";
+ L["Elemental"] = "Elementar";
+ L["Enhancement"] = "Melhoria";
+ --Warlock
+ L["Warlock"] = "Feiticeiro";
+ L["Affliction"] = "Aflição";
+ L["Demonology"] = "Demonologia";
+ L["Destruction"] = "Destruição";
+ --Hunter
+ L["Hunter"] = "Caçador";
+ L["Beast Mastery"] = "Maestria da Besta";
+ L["Marksmanship"] = "Pontaria";
+ L["Survival"] = "Sobrevivência";
+ --Paladin
+ L["Paladin"] = "Paladino";
+ L["Retribution"] = "Retribuição";
+  --Mage
+ L["Mage"] = "Mago";
+ L["Arcane"] = "Arcano";
+ L["Fire"] = "Fogo";
+  --Shared-Spec-names
+ L["Frost"] = "Geada";
+ L["Restoration"] = "Restauração";
+ L["Holy"] = "Santo";
+ L["Protection"] = "Proteção";
+ --Side-Buttons
+ L["Reset Class"] = "Classe reset";
+ L["Title_Class"] = "Classe de texto de habilidade de rolagem";
+end
