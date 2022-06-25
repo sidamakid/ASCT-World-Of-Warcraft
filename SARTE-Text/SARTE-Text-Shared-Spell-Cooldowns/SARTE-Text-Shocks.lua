bb4acfee621d7bf65886bf9978631744889d6bc5
sidamakid <38231721+sidamakid@users.noreply.github.com>
2022-06-25 14:06:27 -0700

    Add files via upload

--- SARTE-Text/SARTE-Text-Shared-Spell-Cooldowns/SARTE-Text-Shocks.lua
+++ SARTE-Text/SARTE-Text-Shared-Spell-Cooldowns/SARTE-Text-Shocks.lua
@@ -10,7 +10,7 @@ f:SetScript("OnEvent", --Run when our event fires
    function(self, event, unit, _, spellName)
     local spellName = GetSpellInfo(spellName)
       if unit == "player" and
-      SpellTableShamanShocks[spellName] and SARTESPELLDB["Elemental"]["Shocks"] == true 
+      SpellTableShamanShocks[spellName] and SARTESPELLDB["Elemental"]["Shocks"] == true
       then
          local spellFrame = _G[SARTE..spellName] or CreateFrame("Frame", SARTE..spellName) --Make a frame whose name is the name of the addon + the name of the spell so it will be unique and safe
          spellFrame:SetScript("OnUpdate", --Run forever!
